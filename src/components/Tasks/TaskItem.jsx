7407eb2bef5b4a43ae950a4bbecc95c4f60abd1d
Susmoy Sen Gupta <susmoy.gupta@welldev.io>
2023-09-13 17:34:36 +0600

    feat: Add functionality to delete tasks

--- src/components/Tasks/TaskItem.jsx
+++ src/components/Tasks/TaskItem.jsx
@@ -1,14 +1,25 @@
+import Button from "@components/ui/form/button";
+import { deleteTask } from "@features/task/taskSlice";
 import { formatDate } from "@utils/formatDate.js";
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
 import "./style.scss";
 
 function TaskItem({ task = {} }) {
-    const { title, createdAt } = task;
+    const { id, title, createdAt } = task;
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        dispatch(deleteTask(id));
+    };
 
     return (
         <div className="task-list__item">
             <h2>{title}</h2>
             <span>Created At: {formatDate(createdAt)}</span>
+            <div className="task-list__action">
+                <Button onClick={handleDelete}>Delete</Button>
+            </div>
         </div>
     );
 }
