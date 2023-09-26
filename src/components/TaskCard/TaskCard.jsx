1f27026261995fc6c705d57df3f3ac25b0b5798d
Susmoy Sen Gupta <susmoy.gupta@welldev.io>
2023-09-26 14:27:23 +0600

    Feat: Purification & Enter Key press Save

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
 import { Button, TextArea } from "@components/ui/form";
 import IconButton from "@components/ui/form/IconButton";
 import { CheckIcon, PencilIcon, TrashIcon } from "@components/ui/icons";
 import { deleteTask, markAsDone, updateTask } from "@features/task/taskSlice";
+import { ENTER_KEY } from "@helpers/constants";
 import formatDate from "@helpers/formatting/formatDate";
 import { compareDates } from "@helpers/operations/compareDates";
+import purify from "@helpers/text/purify";
 import "@components/TaskCard/TaskCard.scss";
 
-function TaskCard({ task, isEditing, onToggleEditing }) {
+function TaskCard({ task }) {
     const { id, title, createdAt, isCompleted, completedAt } = task;
     const dispatch = useDispatch();
+
+    const [isEditing, setIsEditing] = useState(false);
     const [editTitle, setEditTitle] = useState(title);
 
     const now = new Date().toISOString();
@@ -23,31 +27,47 @@ function TaskCard({ task, isEditing, onToggleEditing }) {
         setEditTitle(event.target.value);
     };
 
-    const editTask = () => {
-        dispatch(updateTask({ id, editTitle }));
+    const turnOffEditing = () => {
+        setIsEditing(false);
     };
 
-    const handleToggleEdit = () => {
-        if (isEditing) {
-            editTask();
+    const editTask = () => {
+        const purifiedEditTitle = purify(editTitle);
+
+        if (purifiedEditTitle === "") {
+            setEditTitle("");
+            return;
         }
-        onToggleEditing(id);
+
+        dispatch(updateTask({ id, editTitle: purifiedEditTitle }));
+        turnOffEditing();
+        setEditTitle(purifiedEditTitle);
     };
 
     const handleDelete = () => {
         if (isEditing) {
-            onToggleEditing(id);
+            turnOffEditing();
             return;
         }
         dispatch(deleteTask(id));
     };
 
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
     const handleMarkAsDone = () => {
         if (isEditing) {
             editTask();
-            onToggleEditing(id);
         }
         dispatch(markAsDone(id));
+    };
+
+    const handleTextareaKeyDown = (event) => {
+        if (event.key === ENTER_KEY && !event.shiftKey) {
+            event.preventDefault();
+            editTask();
+        }
     };
 
     return (
@@ -58,14 +78,20 @@ function TaskCard({ task, isEditing, onToggleEditing }) {
             })}
         >
             {isEditing ? (
-                <TextArea className="task-card__input" value={editTitle} onChange={handleTitleChange} />
+                <TextArea
+                    className="task-card__input"
+                    value={editTitle}
+                    onChange={handleTitleChange}
+                    onKeyUp={handleTextareaKeyDown}
+                />
             ) : (
                 <h2 className="task-card__title">{title}</h2>
             )}
             <span>Created At: {formatDate(createdAt)}</span>
             <div className="task-card__body">
                 <div className="task-card__actions-wrapper">
-                    {isEditing && <Button onClick={handleToggleEdit}>Save</Button>}
+                    {isEditing && <Button onClick={editTask}>Save</Button>}
+
                     {!isCompleted && (
                         <IconButton onClick={handleMarkAsDone}>
                             <CheckIcon />
@@ -73,7 +99,7 @@ function TaskCard({ task, isEditing, onToggleEditing }) {
                     )}
 
                     {!isEditing && !isCompleted && (
-                        <IconButton onClick={handleToggleEdit}>
+                        <IconButton onClick={handleEditClick}>
                             <PencilIcon />
                         </IconButton>
                     )}
@@ -97,8 +123,6 @@ TaskCard.defaultProps = {
 };
 
 TaskCard.propTypes = {
-    isEditing: PropTypes.bool.isRequired,
-    onToggleEditing: PropTypes.func.isRequired,
     task: PropTypes.shape({
         id: PropTypes.string,
         title: PropTypes.string,
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,37 +1,20 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import TaskCard from "@components/TaskCard";
 import CreateTaskCard from "@components/CreateTaskCard";
 import "./TaskList.scss";
 
-const INVALID_EDITING_ID = -1;
-
 function TaskList({ isTaskCreating, onTaskCreation }) {
     const tasks = useSelector((state) => state.tasks.tasks);
-    const [editingId, setEditingId] = useState(INVALID_EDITING_ID);
 
-    const toggleEditing = (taskId) => {
-        setEditingId((prevEditingId) => {
-            return prevEditingId === INVALID_EDITING_ID ? taskId : INVALID_EDITING_ID;
-        });
-    };
-
-    const taskItems = tasks.map((task) => {
-        return (
-            <TaskCard
-                key={task.id}
-                task={task}
-                isEditing={Boolean(editingId === task.id)}
-                onToggleEditing={toggleEditing}
-            />
-        );
+    const taskCards = tasks.map((task) => {
+        return <TaskCard key={task.id} task={task} />;
     });
 
     return (
         <div className="task-grid">
             {isTaskCreating && <CreateTaskCard onTaskCreation={onTaskCreation} />}
-            {taskItems}
+            {taskCards}
         </div>
     );
 }
