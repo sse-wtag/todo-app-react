59e441f5a12029137bdb94c6d68f655c4d2bca27
Susmoy Sen Gupta <susmoy.gupta@welldev.io>
2023-10-05 12:07:01 +0600

    Setup Vitest, RTL

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,9 @@ export default defineConfig({
             "@utils": path.resolve(__dirname, "./src/utils/"),
         },
     },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./testSetup.js",
+    },
 });
