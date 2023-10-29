ab9358db807daf12e172d3fcbec290ac0c4f9832
Susmoy Sen Gupta <susmoy.gupta@welldev.io>
2023-10-29 10:32:40 +0600

    Refactor: change state value to capital

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,6 @@
 export const ENTER_KEY = "Enter";
 export const TASK_PER_PAGE = 9;
 
-export const TASK_STATE_ALL = "all";
-export const TASK_STATE_COMPLETE = "complete";
-export const TASK_STATE_INCOMPLETE = "incomplete";
+export const TASK_STATE_ALL = "All";
+export const TASK_STATE_COMPLETE = "Complete";
+export const TASK_STATE_INCOMPLETE = "Incomplete";
